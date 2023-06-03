7d9030c0a40a5485338aba876370387cba9cf1d0
axtr897 <albinxtr897@gmail.com>
2023-06-03 15:41:35 +0530

    adding swipe from here come back if error came

--- app/src/main/java/abc/sadnoxx/hashtaggenerator/fragments/hashtags/CardDataRepository.kt
+++ app/src/main/java/abc/sadnoxx/hashtaggenerator/fragments/hashtags/CardDataRepository.kt
@@ -1,12 +1,8 @@
 package abc.sadnoxx.hashtaggenerator.fragments.hashtags
 
-import abc.sadnoxx.hashtaggenerator.fragments.hashtags.Card
 
 object CardDataRepository {
     val cardDataList = listOf(
-        Card("Card 1", listOf("tag1", "tag2", "tag3")),
-        Card("Card 2", listOf("tag4", "tag5")),
-        Card("Card 3", listOf("tag6", "tag7", "tag8", "tag9")),
         Card(
             "Viral", listOf(
                 "#viral", "#trending", "#viralvideos", "#explorepage", "#explore",
@@ -16,7 +12,40 @@ object CardDataRepository {
                 "#trendingnow", "#viralindonesia", "#reelitfeelit", "#viralreels",
                 "#exploremore", "#instadaily", "#views", "#post", "#memes", "#india"
             )
+        ),
+        Card(
+            "Reels", listOf(
+                "#reels", "#reelsinstagram", "#reelitfeelit", "#reelsvideo", "#reelsindia",
+                "#reel", "#reelkarofeelkaro", "#instagramreels", "#trending", "#instareels",
+                "#reelsinsta", "#viral", "#reelsviral", "#instagram", "#viralreels",
+                "#reelsofinstagram", "#feelitreelit", "#trendingreels", "#feelkaroreelkaro",
+                "#explore", "#foryou", "#reelinstagram", "#reelvideo", "#explorepage",
+                "#tiktok", "#reeltoreel", "#reelsbrasil", "#reelit", "#instagood",
+                "#reelskarofeelkaro"
+            )
+        ),
+        Card(
+            "Dog", listOf(
+                "#dog", "#dogsofinstagram", "#dogs", "#puppy", "#doglover", "#dogstagram",
+                "#doglife", "#dogoftheday", "#instadog", "#doglovers", "#puppylove", "#pet",
+                "#puppies", "#puppiesofinstagram", "#doggo", "#hund", "#dogphotography",
+                "#doglove", "#dogsofinsta", "#doggy", "#ilovemydog", "#pets", "#pup",
+                "#petstagram", "#cutedog", "#chien", "#dogs_of_instagram", "#犬", "#dogslife",
+                "#puppylife"
+            )
+        ),
+        Card(
+            "Workfromhome", listOf(
+                "#workfromhome", "#affiliatemarketing", "#affiliatemarketer", "#sidehustles",
+                "#financialfreedom", "#financialliteracy", "#financialeducation", "#passiveincome",
+                "#financialgoals", "#laptoplifestyle", "#bestaffiliateprogram", "#singlemom",
+                "#singleparent", "#singledad", "#singlemoms", "#instagood", "#instagram",
+                "#followme", "#earnfromhome", "#workfromhomemom", "#extraincome", "#sidehustleideas",
+                "#sidehustle", "#earnextraincome", "#changeyourlifetoday", "#makemoneyonline",
+                "#sidehustlesuccess", "#sidebusiness", "#debtfreecommunity", "#cashflowingassets"
+            )
         )
-        // Add more CardData objects as needed
+
+
     )
 }
--- app/src/main/java/abc/sadnoxx/hashtaggenerator/fragments/hashtags/HashtagsFragment.kt
+++ app/src/main/java/abc/sadnoxx/hashtaggenerator/fragments/hashtags/HashtagsFragment.kt
@@ -1,6 +1,7 @@
 package abc.sadnoxx.hashtaggenerator.fragments.hashtags
 
 import abc.sadnoxx.hashtaggenerator.R
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -8,16 +9,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
+import android.view.inputmethod.InputMethodManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 
 class HashtagsFragment : Fragment() {
 
-    private lateinit var searchBar: EditText
+    private lateinit var searchBar: TextInputEditText
     private lateinit var cardAdapter: CardAdapter
+    private lateinit var searchBarTop: TextInputLayout
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,7 +31,39 @@ class HashtagsFragment : Fragment() {
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_hashtags, container, false)
 
-        searchBar = rootView.findViewById(R.id.searchBar)
+
+        searchBarTop = rootView.findViewById(R.id.searchbartop)
+        val topAppBar: MaterialToolbar = rootView.findViewById(R.id.topAppBar)
+
+        topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.searchItem -> {
+                    if (searchBarTop.visibility == View.VISIBLE) {
+                        searchBarTop.visibility = View.GONE
+                        menuItem.setIcon(R.drawable.zoom_in) // Set the search item icon when searchBarTop is hidden
+                    } else {
+                        searchBarTop.visibility = View.VISIBLE
+                        menuItem.setIcon(R.drawable.zoom_out) // Set the close item icon when searchBarTop is visible
+                    }
+                    true
+                }
+
+                R.id.more -> {
+                    // Handle more item (inside overflow menu) press
+                    true
+                }
+
+                else -> false
+            }
+        }
+
+
+
+
+
+
+
+        searchBar = rootView.findViewById(R.id.search_bar)
         val recyclerView: RecyclerView = rootView.findViewById(R.id.recyclerView)
 
         cardAdapter = CardAdapter(CardDataRepository.cardDataList)
@@ -46,6 +83,20 @@ class HashtagsFragment : Fragment() {
             }
         })
 
+
+        searchBar.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                // Show the keyboard
+                val imm = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.showSoftInput(searchBar, 0)
+            } else {
+                // Hide the keyboard
+                val imm = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(searchBar.windowToken, 0)
+            }
+        }
+
         return rootView
     }
+
 }
