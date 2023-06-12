de88a08172a6a8c3a02ebd69b29ee40a7b0944fa
axtr897 <albinxtr897@gmail.com>
2023-06-12 10:05:30 +0530

    data adding

--- app/src/main/java/abc/sadnoxx/hashtaggenerator/fragments/hashtag/CardDataManager.kt
+++
@@ -1,9 +0,0 @@
-package abc.sadnoxx.hashtaggenerator.fragments.hashtag
-
-import abc.sadnoxx.hashtaggenerator.fragments.hashtag.hashtags.Card
-import android.content.Context
-import android.preference.PreferenceManager
-import org.json.JSONArray
-import org.json.JSONObject
-
-private const val SAVED_CARDS_KEY = "savedCards"
