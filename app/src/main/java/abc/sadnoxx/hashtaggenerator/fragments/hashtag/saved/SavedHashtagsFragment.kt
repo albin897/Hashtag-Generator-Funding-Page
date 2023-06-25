a2209cf459e61fde1ed5d9fda677d2c146511b2a
axtr897 <albinxtr897@gmail.com>
2023-06-25 08:30:52 +0530

    adding copy to another object

--- app/src/main/java/abc/sadnoxx/hashtaggenerator/fragments/hashtag/saved/SavedHashtagsFragment.kt
+++ app/src/main/java/abc/sadnoxx/hashtaggenerator/fragments/hashtag/saved/SavedHashtagsFragment.kt
@@ -14,7 +14,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -30,7 +29,6 @@ class SavedHashtagsFragment : Fragment() {
     private lateinit var savedCardAdapter: SavedCardAdapter
     private val savedCards: MutableList<Card> = mutableListOf()
     private lateinit var sharedPreferences: SharedPreferences
-    private lateinit var container1: FrameLayout
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -43,13 +41,12 @@ class SavedHashtagsFragment : Fragment() {
 
         recyclerView = rootView.findViewById(R.id.savedRecyclerView)
         savedCardAdapter = SavedCardAdapter(savedCards, this::copyToClipboard, this::removeCard)
-        container1 = rootView.findViewById(R.id.container)
         recyclerView.adapter = savedCardAdapter
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
         loadSavedCards()
 
-        toffeeView(container)
+
 
 
         return rootView
@@ -121,19 +118,6 @@ class SavedHashtagsFragment : Fragment() {
     }
 
 
-    private fun toffeeView(container: ViewGroup?) {
-        // Step 1: Check if there are no items in the adapter
-        if (savedCardAdapter.itemCount == 0) {
-            // Step 2: Create the layout for the placeholder
-            val placeholderView =
-                layoutInflater.inflate(R.layout.placeholder_layout, container, false)
-
-            container1.addView(placeholderView)
-        } else {
-            // Step 5: Remove the placeholder layout if there are items available
-            container1.removeAllViews()
-        }
-    }
 
 
 }
